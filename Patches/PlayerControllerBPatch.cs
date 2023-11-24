2bebba80115af2c5d10afef09f39eb65200240ba
Levi <17068563+LeviTheOtaku@users.noreply.github.com>
2023-11-24 14:53:55 +0100

    first upload :D

---
+++ Patches/PlayerControllerBPatch.cs
@@ -0,0 +1,37 @@
+﻿using BepInEx.Logging;
+using GameNetcodeStuff;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace LethalWidescreen.Patches
+{
+    [HarmonyPatch(typeof(PlayerControllerB))]
+    internal class PlayerControllerBPatch
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch("OnEnable")]
+        private static void OnEnable()
+        {
+            GameObject ingamePlayerHud = GameObject.Find("Panel");
+            RectTransform panelRectTransform = ingamePlayerHud.GetComponent<RectTransform>();
+
+            float screenWidth = Screen.width;
+            float screenHeight = Screen.height;
+
+            panelRectTransform.sizeDelta = new Vector2(screenWidth / 8, screenHeight / 8);
+
+            panelRectTransform.anchoredPosition = new Vector2(0, 0);
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch("Update")]
+        private static void LateUpdate(PlayerControllerB __instance)
+        {
+            Camera camera = __instance.gameplayCamera;
+            camera.fieldOfView = __instance.targetFOV*1.33f;
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,33 @@
+﻿using BepInEx;
+using System.Collections;
+using UnityEngine;
+using HarmonyLib;
+using LethalWidescreen.Patches;
+using System.Collections.Generic;
+using System.Linq;
+using BepInEx.Logging;
+using GameNetcodeStuff;
+
+namespace LethalWidescreen
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class LethalWidescreen : BaseUnityPlugin
+    {
+        internal static ManualLogSource mls;
+        private readonly Harmony harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+        private static LethalWidescreen Instance;
+
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            mls = BepInEx.Logging.Logger.CreateLogSource(PluginInfo.PLUGIN_GUID);
+
+            mls.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+            harmony.PatchAll();
+        }
+    }
+}
